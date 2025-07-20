444d8d18afbe37f043ff5cc91526eaf13b295117
SMhrineel8 <91449112+SMhrineel8@users.noreply.github.com>
2025-07-20 15:19:58 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{ts,tsx}"
-  ],
-  theme: {
-    extend: {},
-  },
+  content: ["./index.html","./src/**/*.{ts,tsx}"],
+  theme: { extend: {} },
   plugins: [],
 };
